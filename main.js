0af0e20df825e986a6f5bedb11a0193ab2cf24ca
Carlos Anderson <carlosjanderson@gmail.com>
2016-11-27 17:39:24 -0400

    Implement hexagonal grid

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ var grid = [];
 var WIDTH = 200;
 var HEIGHT = 100;
 
-var CELL_WIDTH = 5
-var CELL_HEIGHT = 5
+var CELL_R = 2
 
-var RULES = {born: [3], survives: [2, 3]};
+var RULES = {born: [2], survives: [3, 4]};
 
-var DELAY = 100;
+var DELAY = 0;
 
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
@@ -32,30 +31,17 @@ function updateGrid() {
     for (var i = 0; i < WIDTH; i++) {
         newGrid[i] = []
         for (var j = 0; j < HEIGHT; j++) {
-            var neighborCount = 0;
-            for (var a = -1; a <= 1; a++) {
-                for (var b = -1; b <= 1; b++) {
-                    if (a == 0 && b == 0)
-                        continue;
-                    if (i + a < 0 || i + a >= WIDTH)
-                        continue;
-                    if (j + b < 0 || j + b >= HEIGHT)
-                        continue;
-                    if (grid[i + a][j + b].alive) {
-                        neighborCount++
-                    }
-                }
-            }
+            var nc = neighborCountHex(grid, i, j);
 
             newGrid[i][j] = {alive: grid[i][j].alive};
 
             if (grid[i][j].alive) {
                 newGrid[i][j].alive =
-                    RULES.survives.indexOf(neighborCount) != -1;
+                    RULES.survives.indexOf(nc) != -1;
             }
             else {
                 newGrid[i][j].alive =
-                    RULES.born.indexOf(neighborCount) != -1;
+                    RULES.born.indexOf(nc) != -1;
             }
         }
     }
@@ -68,6 +54,47 @@ function updateGrid() {
     window.requestAnimationFrame(updateGrid);
 }
 
+function neighborCount(grid, i, j) {
+    var neighborCount = 0;
+    for (var a = -1; a <= 1; a++) {
+        for (var b = -1; b <= 1; b++) {
+            if (a == 0 && b == 0)
+                continue;
+            if (i + a < 0 || i + a >= WIDTH)
+                continue;
+            if (j + b < 0 || j + b >= HEIGHT)
+                continue;
+            if (grid[i + a][j + b].alive) {
+                neighborCount++
+            }
+        }
+    }
+    return neighborCount;
+}
+
+function neighborCountHex(grid, i, j) {
+    var neighbors = [
+        [i, j - 1], [i, j + 1],
+        [i - 1, j], [i + 1, j],
+    ]
+
+    if (j % 2 == 1) {
+        neighbors[4] = [i + 1, j - 1];
+        neighbors[5] = [i + 1, j + 1];
+    }
+    else {
+        neighbors[4] = [i - 1, j - 1];
+        neighbors[5] = [i - 1, j + 1];
+    }
+
+    var neighbors = neighbors.filter(
+            function(x) { return x[0] >= 0 && x[0] < WIDTH &&
+                                 x[1] >= 0 && x[1] < HEIGHT &&
+                                 grid[x[0]][x[1]].alive; });
+
+    return neighbors.length;
+}
+
 function sleep(milliseconds) {
       var start = new Date().getTime();
         for (var i = 0; i < 1e7; i++) {
@@ -78,19 +105,40 @@ function sleep(milliseconds) {
 }
 
 function drawGrid() {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, WIDTH * 2 * CELL_R, HEIGHT * 2 * CELL_R);
+    context.fillStyle = "black"
+
     for (var i = 0; i < WIDTH; i++) {
         for (var j = 0; j < HEIGHT; j++) {
             if (grid[i][j].alive) {
-                context.fillStyle = "black"
+                drawCellAsCircle(context, i, j);
             }
-            else {
-                context.fillStyle = "white"
-            }
-            context.fillRect(
-                i * CELL_WIDTH + i,
-                j * CELL_HEIGHT + j, CELL_WIDTH, CELL_HEIGHT);
         }
     }
+}
+
+function drawCellAsRect(context, i, j) {
+    context.fillRect(
+        i * CELL_R,
+        j * CELL_R, CELL_R, CELL_R);
+}
+
+function drawCellAsCircle(context, i, j) {
+    context.beginPath();
+    if (j % 2 == 0) {
+        context.arc(i * 2 * CELL_R + CELL_R,
+                    j * 2 * CELL_R + CELL_R - (2 - CELL_R * Math.sqrt(2)),
+                    CELL_R,
+                    0, 2 * Math.PI);
+    }
+    else {
+        context.arc(i * 2 * CELL_R + CELL_R + CELL_R,
+                    j * 2 * CELL_R + CELL_R - (2 - CELL_R * Math.sqrt(2)),
+                    CELL_R,
+                    0, 2 * Math.PI);
+    }
+    context.fill();
 }
 
 init();
